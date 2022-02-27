021c5dbde57f419fe3b01490cb88f33114297a3d
Cutano <rinne@rinne.top>
2022-02-27 23:30:38 +0800

    Update index.js

--- src/views/root/index.js
+++ src/views/root/index.js
@@ -1,14 +1,10 @@
 import { Button } from '@mui/material';
 
-const RootPage = () => {
-    const a = ' ';
-
-    return (
-        <div>
-            <Button href="/login">登录</Button>
-            <Button href="/register">注册</Button>
-        </div>
-    );
-};
+const RootPage = () => (
+    <div>
+        <Button href="/login">登录</Button>
+        <Button href="/register">注册</Button>
+    </div>
+);
 
 export default RootPage;
