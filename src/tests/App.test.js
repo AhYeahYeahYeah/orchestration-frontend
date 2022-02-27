813dc84b45cf93a5ad6ff291ba56df909131e9f3
Cutano <rinne@rinne.top>
2022-02-27 23:26:03 +0800

    Create App.test.js

---
+++ src/tests/App.test.js
@@ -0,0 +1,9 @@
+import { render } from '@testing-library/react';
+
+// eslint-disable-next-line no-undef
+test('renders learn react link', () => {
+    render(<div>abc</div>);
+    // const linkElement = screen.getByText(/abc/i);
+    // eslint-disable-next-line no-undef
+    // expect(linkElement).toBeInTheDocument();
+});
