c8572118c107604a38f9e5336c603c6cf00dfc32
1010353663 <1010353663@qq.com>
2022-03-20 19:13:56 +0800

    add style

--- src/views/pages/authentication/AuthWrapper1.js
+++ src/views/pages/authentication/AuthWrapper1.js
@@ -3,8 +3,8 @@ import { styled } from '@mui/material/styles';
 
 // ==============================|| AUTHENTICATION 1 WRAPPER ||============================== //
 
-const AuthWrapper1 = styled('div')(({ theme }) => ({
-    backgroundColor: theme.palette.primary.light,
+const AuthWrapper1 = styled('div')(() => ({
+    backgroundColor: '#e0e0e0',
     minHeight: '100vh'
 }));
 
