289bf12363a82940f086e5cd86bc8a78036cddfe
1010353663 <1010353663@qq.com>
2022-04-09 21:54:44 +0800

    add search

--- src/views/product/ControllableStates.js
+++ src/views/product/ControllableStates.js
@@ -2,6 +2,8 @@ import * as React from 'react';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 import PropTypes from 'prop-types';
+import { Grid, IconButton } from '@mui/material';
+import { Search } from '@mui/icons-material';
 
 // const options = ['新建', '模板1'];
 
@@ -11,25 +13,42 @@ export default function ControllableStates({ workName, updateFid, updateproducts
     const [value, setValue] = React.useState(updateproductsingle === null ? null : updateproductsingle.workName);
     const [inputValue, setInputValue] = React.useState('');
 
+    function lookFlow() {
+        console.log(value);
+        if (value !== null && value !== '') {
+            window.open(`http://conductor.rinne.top:5000/workflowDef/${value}`);
+        }
+    }
     return (
         <div>
-            <Autocomplete
-                value={value}
-                onChange={(event, newValue) => {
-                    setValue(newValue);
-                    updateFid(newValue);
-                    // eslint-disable-next-line react/prop-types
-                    if (updateproductsingle !== null) updateproductsingle.workName = newValue;
-                }}
-                inputValue={inputValue}
-                onInputChange={(event, newInputValue) => {
-                    setInputValue(newInputValue);
-                }}
-                id="controllable-states-demo"
-                options={workName}
-                sx={{ width: '100%' }}
-                renderInput={(params) => <TextField {...params} label="请选择模板" />}
-            />
+            <Grid container spacing={1}>
+                <Grid item xs={10}>
+                    <Autocomplete
+                        value={value}
+                        onChange={(event, newValue) => {
+                            setValue(newValue);
+                            updateFid(newValue);
+                            // eslint-disable-next-line react/prop-types
+                            if (updateproductsingle !== null) updateproductsingle.workName = newValue;
+                        }}
+                        inputValue={inputValue}
+                        onInputChange={(event, newInputValue) => {
+                            setInputValue(newInputValue);
+                        }}
+                        id="controllable-states-demo"
+                        options={workName}
+                        sx={{ width: '100%' }}
+                        renderInput={(params) => <TextField {...params} label="请选择模板" />}
+                    />
+                </Grid>
+                <Grid item xs={1}>
+                    <IconButton position="end" onClick={() => lookFlow()}>
+                        {/* eslint-disable-next-line jsx-a11y/media-has-caption */}
+                        {/* <audio /> */}
+                        <Search />
+                    </IconButton>
+                </Grid>
+            </Grid>
         </div>
     );
 }
